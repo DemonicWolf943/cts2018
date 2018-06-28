6557f08a98026f5619599e122f2515c5fea33c76
Code <Code@batc.local>
2018-06-28 08:35:01 -0600

    Initial commit

---
+++ HTMLandCSS/PackageProject/src/bootstrap.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+// const twoWeeksFromNow = moment().add(12354, 'years');
+
+// console.log(twoWeeksFromNow.toString());
+
+const sixMonthsAgo = moment().subtract(6, 'months')
+console.log(sixMonthsAgo.toString());
