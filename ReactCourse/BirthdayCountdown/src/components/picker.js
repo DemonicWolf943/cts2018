c0fcf46845ea9b04adf5192f284a8a09fca377c4
Code <Code@batc.local>
2018-07-03 12:01:41 -0600

    7/3

---
+++ ReactCourse/BirthdayCountdown/src/components/picker.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default class Picker extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			startDate: moment()
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(date) {
+		console.log('trying to change date');
+		this.setState({
+			startDate: date
+		});
+	}
+
+	render() {
+		return (
+			<div className = 'picker'>
+				<DatePicker
+					selected={this.state.startDate}
+					onChange={this.handleChange}
+				/>
+			</div>
+		);
+	}
+}
